c0e01da97aff9c8dc72419e0b79392895e0e68de
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-03 18:29:08 -0300

    print comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for a in range(100):
     if a == 99:
-        print("{0}".format(a), end='\n')
-    elif a >= 0 and a <= 9:
-        print("0{0}".format(a), end=', ')
+        print("{:02d}".format(a), end='\n')
     else:
-        print("{0}".format(a), end=', ')
+        print("{:02d}".format(a), end=', ')
