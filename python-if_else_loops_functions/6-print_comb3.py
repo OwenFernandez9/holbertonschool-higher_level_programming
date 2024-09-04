331f320828b032427f82cfe35dc6e554990a3e9e
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-04 12:55:42 -0300

    print comb

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for a in range(9):
     for b in range(a + 1, 10):
         if a == 8 and b == 9:
-            print(f"{a}{b}")
+            print("{}{}".format(a, b))
         else:
-            print(f"{a}{b}", end=', ')
+            print("{}{}".format(a, b), end=', ')
+    
