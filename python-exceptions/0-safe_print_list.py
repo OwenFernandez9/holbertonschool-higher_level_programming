1b5b96a04834b028dbc7bf758f71d46f831951e5
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-09 17:00:13 -0300

    safe print list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for a in range(x):
+            print(my_list[a], end=" ")
+        print()
+        return a + 1
+    except andexError:
+        print()
+        return a
