583f66afad3750597d597a30cba919af92de5e0a
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-09 15:05:12 -0300

    delete at

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 and idx > my_list:
+        return my_list
+    new_list = my_list
+    new_list.remove(new_list[idx])
+    return new_list
