353f4a1e495f7f283d89ba0bb2d6e0d0ea2cdfec
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-11 19:02:16 -0300

    best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    maxkey, max = list(a_dictionary.items())[0]
+    for key, value in a_dictionary.items():
+        if value > max:
+            max = value
+            maxkey = key
+    return maxkey
