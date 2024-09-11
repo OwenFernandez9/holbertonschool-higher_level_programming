98e763dec5b928e60a83bda50ef23c712d46fe98
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-11 14:23:50 -0300

    uniq add

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,8 @@
 
 def uniq_add(my_list=[]):
     sum_elements = 0
+    my_list = set(my_list)
+    my_list = list(my_list)
     for element in my_list:
         sum_elements += element
-    return(sum_elements)
+    return (sum_elements)
