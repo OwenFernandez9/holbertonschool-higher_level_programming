64e0dd046c343a5c73ba1861594cd2323ced1145
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-12 11:53:43 -0300

    add integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,7 +3,16 @@
 A function that adds 2 integers.
 """
 
+
 def add_integer(a, b=98):
+    """
+    The code attempts to convert the variables a and b to integers.
+    If the conversion fails for a, it raises a TypeError with the message
+        "a must be an integer".
+    If the conversion fails for b, it raises a TypeError with the message
+        "b must be an integer".
+    If both conversions are successful, it returns the sum of a and b.
+    """
     try:
         a = int(a)
     except Exception:
