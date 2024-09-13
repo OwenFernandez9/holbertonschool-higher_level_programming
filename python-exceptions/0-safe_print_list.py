b83304db95d7866207b9f664d20575bca9bb55b8
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-13 15:56:57 -0300

    safe print list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,6 @@
 
 def safe_print_list(my_list=[], x=0):
     a = 0
-    if not my_list:
-        return None
-    if x == 0:
-        return 0
-    
     try:
         for elem in my_list:
             if a >= x:
