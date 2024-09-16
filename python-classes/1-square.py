962e4f56b052d63905dbc02593ec8742a44e8709
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-16 10:23:41 -0300

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script defines a class called Square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+    def __init__(self, size):
+        """
+        initialize the size
+        """
+        self.__size = size
