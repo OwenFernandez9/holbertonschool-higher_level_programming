25271fe29d06c6bfefd0143653bf61ac93a6f0b4
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-16 11:53:38 -0300

    square

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script defines a class called Square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+    def __init__(self, size=0):
+        """
+        initialize the size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
