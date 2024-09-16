e385fe622698fd5694a7bfb656761612891a8322
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-16 13:33:46 -0300

    square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,6 +46,8 @@ class Square:
         self.__position = value
 
     def my_print(self):
+        if self.__size == 0:
+            return
         for a in range(self.__position[1]):
             print()
         for b in range(self.__size):
