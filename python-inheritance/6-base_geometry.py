8050331b3c83547b048221aaff4592a0e9803342
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-23 18:25:12 -0300

    base geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This script defines a class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Represents a base geometry.
+    """
+    def area(Self):
+        raise Exception("area() is not implemented")
