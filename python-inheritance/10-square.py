3e615a0eef2f5676f49bed125ab9a47c06524da2
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-24 12:09:11 -0300

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script defines a class called Square.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    Represents a Square.
+    """
+    def __init__(self, size):
+         self.integer_validator("size", size)
+         self.__size = size
+
+    def area(self):
+         return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
