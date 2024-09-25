7754be6a031d44746a4ec9fe9ab88529117406a6
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-25 19:12:59 -0300

    my list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-
+this module contains a class called Mylist
 """
 
 
 class MyList(list):
     """
-
+    a class MyList that inherits from list
     """
     def print_sorted(self):
         number = list(self)
