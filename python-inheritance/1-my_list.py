ac356e6d828f931c2584dd50e65ba25332412d74
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-25 19:04:39 -0300

    my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+class MyList(list):
+    """
+
+    """
+    def print_sorted(self):
+        number = list(self)
+        number = sorted(number)
+        print(number)
+
