c73977e883fa38d843347f22dd7fb8cf43f93d1a
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-30 10:47:40 -0300

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,9 @@
 in te module declarated the function read_file
 """
 def read_file(filename=""):
+    """
+    This function open and read file
+    """
     with open(filename, 'r') as file:
         content = file.read()
         print(content)
