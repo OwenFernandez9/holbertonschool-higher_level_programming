d98eb673c084ed85b89e885bebd72b25c668ddc3
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-30 12:25:35 -0300

    from json string

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -7,7 +7,7 @@ import json
 
 def from_json_string(my_str):
     """
-    This function converts the object into a string
+    This function converts the string into a object
     """
     json_obj = json.loads(my_str)
     return json_obj
