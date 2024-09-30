ba89035ca6ab30066b4a5533efa0400b3a2b95bc
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-30 12:15:07 -0300

    json string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,11 +2,12 @@
 """
 in the module the to_json_string function is declared
 """
+import json
 
 
 def to_json_string(my_obj):
     """
     This function converts the object into a string
     """
-    obj_str = str(my_obj)
+    obj_str = json.dumps(my_obj)
     return obj_str
