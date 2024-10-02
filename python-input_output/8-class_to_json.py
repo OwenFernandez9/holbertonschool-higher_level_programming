91917fd1227b235de718f6298be8494d6ccfd82a
OwenFernandez9 <9357@holbertonstudents.com>
2024-10-02 16:58:44 -0300

    class to json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""
+In te module the function class_to_json is declarated
+"""
 
 
 def class_to_json(obj):
+    """
+    a function that returns the dictionary/
+      description with simple data structure/
+          (list, dictionary, string, integer and boolean)/
+            for JSON serialization of an object
+    """
     cont = obj.__dict__
     dic = {
         clave: valor
