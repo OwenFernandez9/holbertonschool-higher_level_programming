ef20ccf66a10381ad86fd95a88595c5e56cd5b1c
OwenFernandez9 <9357@holbertonstudents.com>
2024-10-02 17:16:12 -0300

    student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
+In the module the class Students is declarated
 """
 
 
 class Student:
+    """
+    a class Student that defines a student by
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
