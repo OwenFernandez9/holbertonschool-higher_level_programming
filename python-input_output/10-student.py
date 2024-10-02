2f5e0895f11c8b088f5d58206bac2e0ad534f89b
OwenFernandez9 <9357@holbertonstudents.com>
2024-10-02 17:57:16 -0300

    student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+In the module the class Students is declarated
+"""
+
+
+class Student:
+    """
+    a class Student that defines a student by
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+        }
+        elif isinstance(attrs, list):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return {}
