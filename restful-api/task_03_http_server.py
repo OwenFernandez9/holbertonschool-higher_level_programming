afcb7e5861e6356196ce720c0188e9f4936eccbe
OwenFernandez9 <9357@holbertonstudents.com>
2024-10-10 11:21:49 -0300

    task 03 http server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,13 +5,7 @@ import json
 
 class Server(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(b'Hello, this is a simple API!')
-        
-        elif self.path == '/data':
+        if self.path == '/data':
             data = {
                 "name": "John",
                 "age": 30,
@@ -24,11 +18,16 @@ class Server(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json_data.encode('utf-8'))
         
-        elif self.path == '/status':
+        elif self.path == '/info':
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            json_info = json.dumps(info)
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(b'OK')
+            self.wfile.write(json_info.encode('utf-8'))
 
         else:
             self.send_response(404)
