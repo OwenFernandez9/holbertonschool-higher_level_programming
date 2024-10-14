9173eb893ef59e2a0520f87bb2f23d901d54cc93
OwenFernandez9 <9357@holbertonstudents.com>
2024-10-14 11:58:41 -0300

    tas 04 flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+@app.route("/data")
+def get_usernames():
+    username = list(users.keys())
+    return jsonify(username)
+
+@app.route("/status")
+def stats():
+    return "OK"
+
+@app.route("/users/<username>")
+def get_user(username):
+    infouser = users.get(username)
+    if infouser is None:
+        return {"error": "User not found"}
+    return jsonify(infouser)
+
+@app.route("/add_user", methods=["POST"])
+def post_user():
+    create = request.get_json()
+    new_user = create['username']
+    new_name = create['name']
+    new_age = create['age']
+    new_city = create['city']
+    if type(create) is not str:
+        return jsonfy({"error": "User not found"})
+    
+    users[new_user] = {"username": new_user, "name": new_name, "age": new_age, "city": new_city}
+
+    message = {"message": "user added", "user": users[create]}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run()
