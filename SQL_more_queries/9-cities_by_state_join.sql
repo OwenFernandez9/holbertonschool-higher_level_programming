b935eb7570e49bea716fadda06f2f69b8c281ea8
OwenFernandez9 <9357@holbertonstudents.com>
2024-10-29 16:14:30 -0300

    cities by state join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,5 @@
 --  lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id AS city_id, 
-cities.name AS city_name, 
-states.name AS state_name
-
+SELECT city_id AS id, city_name AS name, state_name AS name
 FROM cities
-
-JOIN states ON cities.state_id = states.id
-
-ORDER BY cities.id ASC;
+WHERE state_id = (SELECT id FROM states WHERE name = 'California' OR name = 'Arizona')
+ORDER BY city_id ASC;
