18a962af6bd243ed1c17164f77b3272a7c4e9c5c
OwenFernandez9 <9357@holbertonstudents.com>
2024-10-30 09:44:02 -0700

    count shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+--
+select tv_genre.name as genre
+COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genre
+LEFT JOIN tv_show_genres ON tv_genre.id = tv_show_genres.genre_id
+GROUP BY tv_genre.name
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC;
